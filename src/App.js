6a0b7d35820b507aa2a0a3796e44c20cca7fbcea
JM Ruiz <jmanuel.rar@emeraldigital.com>
2022-08-25 17:34:46 -0500

    feat: cambio mensaje

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <code>Emerald Rules!!</code>
+          HOLA!!!
         </p>
         <a
           className="App-link"
